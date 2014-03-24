4645508933fdd32a31cac5af296cef7e5ce664b6
Elijah Hall <rakshassa@gmail.com>
2014-03-24 17:56:18 -0500

    start rspec

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
+include ApplicationHelper
 
 describe SearchesController do
+	subject { page }
 
+	describe "new" do
+		let(:user) { User.create(name: "testTLX", isTLX: true) }
+		before { visit new_search_path }
+	
+
+
+	end
 end
